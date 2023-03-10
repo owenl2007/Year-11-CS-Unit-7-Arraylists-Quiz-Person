45d37d04e632aafd6fd0d8469fda261c1a6cc43f
owenl2007 <owenl2025@student.cis.edu.hk>
2023-03-10 08:52:59 +0800

    Hello

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,33 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String FirstNameSetter, String LastNameSetter, int AgeSetter){
+        firstName = FirstNameSetter;
+        lastName = LastNameSetter;
+        age = AgeSetter;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        String returnInfo = "First name: " + firstName + ", Last name:" + lastName + ", Age: " + age;
+        return returnInfo;
+    }
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -1,22 +1,44 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PersonFactory {
-
     private ArrayList<Person> allPersons;
-
     public PersonFactory() {
         allPersons = new ArrayList<>();
     }
-
     public void addPerson(Person person) {
+        int initial = allPersons.size();
 
+        for(int i = 0; i < allPersons.size(); i++){
+            String str1 = person.getLastName();
+            String str2 = allPersons.get(i).getLastName();
+            if(str2.compareTo(str1) >= 0)
+                allPersons.add(i, person);
+        }
+
+
+        if(initial == allPersons.size())
+            allPersons.add(person);
     }
 
     public ArrayList<Person> under18() {
+        ArrayList<Person> ageunder18listofPeople = new ArrayList<>();
 
+        for (Person person: allPersons){
+            if(person.getAge() < 18)
+                ageunder18listofPeople.add(person);
+        }
+        return ageunder18listofPeople;
     }
 
+
+
     public boolean checkLastName(String lastName) {
+        for(Person person: allPersons){
+            if (person.getLastName().equals(lastName))
+                return true;
+        }
+        return false;
 
     }
 }
